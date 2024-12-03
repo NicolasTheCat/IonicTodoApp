096285e22d4724203b51fbcd10c487bf3e944831
NicolasTheCat <97517397+NicolasTheCat@users.noreply.github.com>
2024-12-03 09:06:09 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'todoApplication',
+  webDir: 'www'
+};
+
+export default config;
